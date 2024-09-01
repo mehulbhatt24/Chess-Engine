0393a848c480be7d758c29efc73fb1ad935ba40f
Mehul <117798406+mehulbhatt24@users.noreply.github.com>
2024-09-01 23:13:41 +0530

    Update socket.js

--- chess/src/connection/socket.js
+++ chess/src/connection/socket.js
@@ -1,4 +1,4 @@
 import {io} from 'socket.io-client';
-const socket = io.connect('https://chess-engine-ol5o.vercel.app/');
+const socket = io.connect('https://chess-engine-p1yi.onrender.com/');
 
 export{socket}
